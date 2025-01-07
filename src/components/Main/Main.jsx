0adb08ad81b700b79ca390de43470ee576503b88
Asjad Ahmad <asjad@actalyst.ai>
2025-01-08 00:53:23 +0530

    added some design changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -59,25 +59,7 @@ const Main = () => {
                                         <img src={assets.arrow_up_right} alt="" />
                                     </div>
                                 })}
-                                {/* <div className="card"
-                                    onClick={() => setInput("Suggest beautiful places to see on an upcoming road trip")}>
-                                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                                    <img src={assets.compass_icon} alt="" />
-                                </div>
-                                <div className="card"
-                                    onClick={() => setInput("Briefly summarize this concept: urban planning")}>
-                                    <p>Briefly summarize this concept: urban planning</p>
-                                    <img src={assets.bulb_icon} alt="" />
-                                </div>
-                                <div className="card"
-                                    onClick={() => setInput("Brainstorm team bonding activities for our work retreat")}>
-                                    <p>Brainstorm team bonding activities for our work retreat</p>
-                                    <img src={assets.message_icon} alt="" />
-                                </div>
-                                <div className="card" onClick={() => setInput("Tell me about React js and React native")}>
-                                    <p>Tell me about React js and React native</p>
-                                    <img src={assets.code_icon} alt="" />
-                                </div> */}
+                                
                             </div>
                         </div>
                     </>
@@ -103,6 +85,7 @@ const Main = () => {
                 }
                 <div className="main-bottom">
                     <div className="search-box">
+                    <button className='footer-btn'><img src={assets.gallery_icon} alt="" /></button>
                         <textarea rows={rows} onChange={(e) => setInput(e.target.value)}
                             onKeyUp={(e) => {
                                 if (e.key === 'Enter') {
@@ -114,15 +97,12 @@ const Main = () => {
                             placeholder="Enter a prompt here"
                         />
                         <div className="icon-container">
-                            <button><img src={assets.gallery_icon} alt="" /></button>
-                            <button><img src={assets.mic_icon} alt="" /></button>
-                            <button type="submit" onClick={() => onSent()}><img src={assets.send_icon} alt="" /></button>
+                            
+                            {/* <button><img src={assets.mic_icon} alt="" /></button> */}
+                            <button className='footer-btn' type="submit" onClick={() => onSent()}><img src={assets.send_icon} alt="" /></button>
                         </div>
                     </div>
-                    <p className="bottom-info">
-                        Gemini may display inaccurate info, including about people, so double-check its responses.
-                        <a href="#">Your privacy and Gemini Apps</a>
-                    </p>
+                   
                 </div>
             </div>
         </main>
